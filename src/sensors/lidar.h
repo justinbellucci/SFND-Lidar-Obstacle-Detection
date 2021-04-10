a3b32219b177afff9a3d777e3ca53baf55fe49a9
justinbellucci <justin.bellucci@me.com>
2021-04-10 12:18:32 -0600

    increase lidar resolution

--- src/sensors/lidar.h
+++ src/sensors/lidar.h
@@ -94,12 +94,12 @@ struct Lidar
 		groundSlope = setGroundSlope;
 
 		// TODO:: increase number of layers to 8 to get higher resoultion pcd
-		int numLayers = 10;
+		int numLayers = 16;
 		// the steepest vertical angle
 		double steepestAngle =  30.0*(-pi/180);
 		double angleRange = 26.0*(pi/180);
 		// TODO:: set to pi/64 to get higher resoultion pcd
-		double horizontalAngleInc = pi/64;
+		double horizontalAngleInc = pi/90;
 
 		double angleIncrement = angleRange/numLayers;
 
